d775afb7ac0d1b24e9755ce2ccaa89025871aa52
Kieran <kieranphyde@gmail.com>
2024-08-02 18:36:07 +0200

    Add docstrings to band

--- band_site/band/models.py
+++ band_site/band/models.py
@@ -1,11 +1,29 @@
-# band/models.py
 from django.db import models
 
 class BandMember(models.Model):
+    """
+    Represents a member of a band, containing information about their
+    name, instrument, biography, and image.
+
+    Attributes:
+        name (CharField): The full name of the band member, limited to 100 characters.
+        instrument (CharField): The instrument that the band member plays, limited to 100 characters.
+        bio (TextField): A detailed biography of the band member, providing background information.
+        image (ImageField): An image of the band member, uploaded to the 'band_members/' directory.
+
+    Methods:
+        __str__(): Returns a string representation of the band member, specifically their name.
+    """
+
     name = models.CharField(max_length=100)
     instrument = models.CharField(max_length=100)
     bio = models.TextField()
     image = models.ImageField(upload_to='band_members/')
 
     def __str__(self):
+        """
+        Returns:
+            str: The name of the band member.
+        """
         return self.name
+
